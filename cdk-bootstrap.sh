554d5fac95f30f1192f141f8987053aa2114b1da
Vivek Kumar Yadav <vivek.yadav@vista.com>
2025-04-17 16:19:46 +0530

    adding generated cdk app

--- cdk-bootstrap.sh
+++ cdk-bootstrap.sh
@@ -4,3 +4,9 @@ npm install -g aws-cdk
 cdk bootstrap aws://<account-id>/<region>
 #this will create a stack in the account with the name CDKToolkit
 #you will need the right permissions to create the stack
+cdk init 
+#this will create a new cdk project in the current directory
+cd init sample-app --language=pythonP
+#this will create a new cdk project in the current directory with the name sample-app
+#and the language python
+#you can also use other languages like java, csharp, typescript, etc
